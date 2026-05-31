ea0bd51708fee0b002a95dadef4069f6577f76ca
Gloomsfield <amory@acosta.boo>
2026-05-31 00:24:28 -0700

    spawn keys for debug purposes

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -1,7 +1,17 @@
+import { PianoKey } from "@gameobjects/PianoKey.js";
+
 // actual gameplay scene
 export class Game extends Phaser.Scene {
 	constructor() {
 		super("game_scene");
+	}
+
+	create() {
+		this.pianoKeys = [];
+
+		for(let i = 0; i < 24; i++) {
+			this.pianoKeys.push(new PianoKey(this, i * 30, 200));
+		}
 	}
 }
 /*
