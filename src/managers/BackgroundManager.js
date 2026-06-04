c7fe1fd7324b1a32d1e18249ace00391872bfe81
chimpin <cbonardi@ucsc.edu>
2026-06-04 00:23:53 -0700

    Fixed BackgroundManager
    Now scrolling is fixed and the fade towards the red background works great too

--- src/managers/BackgroundManager.js
+++ src/managers/BackgroundManager.js
@@ -9,7 +9,10 @@ export class BackgroundManager {
 
         this.blueFadeStart = 3;
         this.blueFadeDuration = 12;
-        
+        this.redFadeStart = 15;
+
+        this.redSpeed = 30;
+
         this.createLayers();
     }
 
@@ -27,21 +30,40 @@ export class BackgroundManager {
         const blue = this.scene.add.image(x, y, key).setOrigin(0.5).setScale(this.scale).setDepth(depth);
 
         const width = blue.width * this.scale;
+
+        const red = this.scene.add.image(x + width, y, key).setOrigin(0.5).setScale(this.scale).setDepth(depth);
+
+        return { blue, red, width };
     }
 
     scrollBackgrounds(pair, speed) {
         pair.blue.x -= speed;
+        pair.red.x -= speed;
 
         if (pair.blue.x <= -pair.width) {
             pair.blue.x = pair.red.x + pair.width;
         }
 
+        if (pair.red.x <= -pair.width) {
+            pair.red.x = pair.blue.x + pair.width;
+        }
     }
 
     createLayers() {
+        this.red1 = this.createBackgrounds("R1", -200);
+        this.red2 = this.createBackgrounds("R2", -150);
+        this.red3 = this.createBackgrounds("R3", -125);
+
         this.blue1 = this.createBackgrounds("B1", -100);
         this.blue2 = this.createBackgrounds("B2", -100);
 
+        this.red3.blue.setAlpha(0);
+        this.red3.red.setAlpha(0);
+
+        this.layers = [
+            { red: this.red1, blue: this.blue1, speed: 40 },
+            { red: this.red2, blue: this.blue2, speed: 70 }
+        ];
     }
 
     update() {
@@ -51,16 +73,25 @@ export class BackgroundManager {
 
         const speedMultiplier = this.speedMultiplier(movementProgress);
 
+        const blueAlpha = 1 - Math.pow(this.fadeMultiplier(elapsed, this.blueFadeStart, this.blueFadeDuration), 2);
 
-        for (const { blue, speed } of this.layers) {
+        const red3Alpha = this.fadeMultiplier(elapsed, this.redFadeStart, this.redFadeStart);
+
+        for (const { red, blue, speed } of this.layers) {
             const distance = speed * elapsed * speedMultiplier;
 
+            this.scrollBackgrounds(red, distance);
             this.scrollBackgrounds(blue, distance);
 
             blue.blue.setAlpha(blueAlpha);
+            blue.red.setAlpha(blueAlpha);
         }
+
+        const red3Distance = this.redSpeed * elapsed * speedMultiplier;
 
         this.scrollBackgrounds(this.red3, red3Distance);
 
+        this.red3.blue.setAlpha(red3Alpha);
+        this.red3.red.setAlpha(red3Alpha);
     }
 }
