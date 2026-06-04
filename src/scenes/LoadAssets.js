2085faa1ba5432580c9e172a21d3a43a5dd1c31a
chimpin <cbonardi@ucsc.edu>
2026-06-03 18:17:41 -0700

    Background assets
    added background assets

--- src/scenes/LoadAssets.js
+++ src/scenes/LoadAssets.js
@@ -7,8 +7,15 @@ export class LoadAssets extends Phaser.Scene {
 		this.load.audio("a440hz-c4_audio", "./assets/piano-samples/a440hz-c4.mp3");
 
 		this.load.image("piano-key_image", "./assets/piano-art/piano-key.png");
+
 		this.load.image("dino-head", "./assets/dino-art/dino.png");
 		this.load.image("dino-head2", "./assets/dino-art/dino2.png");
+
+		this.load.image("B1", "./assets/background-art/Blue1.png");
+		this.load.image("B2", "./assets/background-art/Blue2.png");
+		this.load.image("R1", "./assets/background-art/Red1.png");
+		this.load.image("R2", "./assets/background-art/Red2.png");
+		this.load.image("R3", "./assets/background-art/Red3.png");
 	}
 }
 
